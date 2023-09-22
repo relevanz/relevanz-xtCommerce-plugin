04b547d6e2b0983c24925c7ab0d3a0b2d4eba4f8
Jens Lukas <jens@adrule.net>
2023-09-22 06:24:12 +0200

    fix issue with frontend not loading in store

--- xt_releva_nz/hooks/page_registry_php_bottom.php
+++ xt_releva_nz/hooks/page_registry_php_bottom.php
@@ -4,4 +4,4 @@
 defined('_VALID_CALL') or die('Direct Access is not allowed.');
 
 
-define('PAGE_TFM_RELEVA_NZ', _SRV_WEB_PLUGINS.'xt_releva_nz/pages/xt_releva_nz.php');
+define('PAGE_XT_RELEVA_NZ', _SRV_WEB_PLUGINS.'xt_releva_nz/pages/xt_releva_nz.php');
